35129492ca56b29c50d11a1bae0811f15daeb08b
rosegithub-dotcom <143061942+rosegithub-dotcom@users.noreply.github.com>
2023-09-07 06:59:20 -0700

    Add files via upload

---
+++ work.py
@@ -0,0 +1,52 @@
+#Sum of even numbers less than 8
+r=2,4,6 # even numbers less than 8
+print(sum(r))
+print("The sum of even numbers less than 8 is" + str(sum(r)))
+
+
+#Average of nunbers
+#Total number of transport maens in our towm
+Taxi = 5
+BodaBoda = 8
+Bykes = 2
+Boats = 6
+Specials =3
+TotalTransport =(Taxi+BodaBoda+Bykes+Boats+Specials)
+Average = (TotalTransport)/5
+print("Their average is " + str(Average))
+
+
+#operators
+-5+8*6
+
+a = -5
+b = 8
+c = 6
+p = (b*c)
+print(a+p)
+
+(55+9)%9
+a = 55
+b = 9
+c = 9
+m = a+b
+print(m%c)
+
+20+-3*5/8
+t = 20
+q = -3
+k = 5
+j = 8
+h = (q*k)/j
+print(t+(h))
+
+5+15/3*2-8%3
+a = 5
+b = 15
+c = 3
+d = 2
+e = -8
+f = 3
+
+p=(5+(15/3)*2)-8
+print(p%f)
